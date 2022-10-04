9df3193c246d5bc59751ad144938140302f8aa83
Armensargsyan76 <76764242az@gmail.com>
2022-10-04 21:26:05 +0400

    login logout costume1

--- src/main/java/com/example/companyemployeespring/controller/UserController.java
+++ src/main/java/com/example/companyemployeespring/controller/UserController.java
@@ -6,9 +6,12 @@ import com.example.companyemployeespring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -25,10 +28,16 @@ public class UserController {
     }
 
     @PostMapping("/user/register")
-    public String userRegister(@ModelAttribute User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole(Role.USER);
-        userRepository.save(user);
+    public String userRegister(@ModelAttribute User user, ModelMap modelMap) {
+        Optional<User> byEmail = userRepository.findByEmail(user.getEmail());
+        if (byEmail.isPresent()) {
+            modelMap.addAttribute("errorMessage", "Email already in use");
+            return "registerUser";
+        } else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRole(Role.USER);
+            userRepository.save(user);
+        }
         return "redirect:/login";
     }
 
